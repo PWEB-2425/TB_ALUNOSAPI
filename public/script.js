e2e492e3fa6a606833c38e0257e931610fdee2b7
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2025-05-06 10:44:58 +0100

    inicial

---
+++ public/script.js
@@ -0,0 +1,46 @@
+// setup da interface
+
+const botaoCria = document.getElementById("ca_cria");
+botaoCria.addEventListener("click", criarAluno);
+
+listarAlunos();
+
+
+async function listarAlunos() {
+    const url = "http://localhost:3000/alunos";
+
+    const resposta = await fetch(url);
+    console.log(resposta);
+
+    const alunosJS = await resposta.json();
+    console.log(alunosJS);
+
+    let ulalunos = document.getElementById("listaA");
+    console.log(ulalunos);
+    ulalunos.innerHTML = "";
+
+    for (aluno of alunosJS) {
+
+        let novoli = document.createElement("li");
+        novoli.innerHTML = aluno.nome + " " + aluno.curso;
+        ulalunos.appendChild(novoli);
+    }
+}
+
+async function criarAluno() {
+    const url = "http://localhost:3000/alunos";
+
+    const alunoJS = {};
+
+    alunoJS.nome = document.getElementById("ca_nome").value;
+    alunoJS.curso = document.getElementById("ca_curso").value;
+    console.log(alunoJS);
+    alunoJSON = JSON.stringify(alunoJS);
+    console.log(alunoJSON);
+
+    const resposta = await fetch(url, { method: "POST", body: alunoJSON });
+
+    listarAlunos();
+
+}
+    
